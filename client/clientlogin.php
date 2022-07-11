8e1023fa5b2931f6157121eb0b906bfb494a8903
monikagore <monikagore6@gmail.com>
2022-07-11 11:12:55 +0530

    changes

---
+++ client/clientlogin.php
@@ -0,0 +1,127 @@
+<?php
+session_start();
+include("config.php");
+if(isset($_POST['login'])){
+$Email=$_POST['email'];
+$Password1=$_POST['password'];
+
+$sql=mysqli_query($conn,"select * from client where Email='$Email'");
+$row=mysqli_fetch_array($sql);
+
+if($row>0){
+    $Email=$row['Email'];
+    $Password=$row['Password'];
+    $hashpassword=password_verify($Password1,$Password);
+    if($hashpassword){
+      $_SESSION['id']=$row['id'];
+
+        header("location:index.php");
+    }else{
+        echo "<script>alert('Password is incorrect');</script>";
+    }
+}
+else{
+    echo "<script>alert('Invalid Email Id');</script>";
+}
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+        <!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.2.0/mdb.min.css"
+  rel="stylesheet"
+/>
+<style>
+            .gradient-custom-2 {
+/* fallback for old browsers */
+background: #fccb90;
+
+/* Chrome 10-25, Safari 5.1-6 */
+background: -webkit-linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
+
+/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
+}
+
+@media (min-width: 768px) {
+.gradient-form {
+height: 100vh !important;
+}
+}
+@media (min-width: 769px) {
+.gradient-custom-2 {
+border-top-right-radius: .3rem;
+border-bottom-right-radius: .3rem;
+}
+}
+</style>
+</head>
+<body>
+
+<section class="h-100 gradient-form" style="background-color: #eee;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-xl-5">
+        <div class="card rounded-3 text-black">
+          <div class="row g-0">
+            <div class="col-lg-12">
+              <div class="card-body p-md-5 mx-md-5">
+
+                <div class="text-center">
+                    <h2>CRM</h2>
+                </div><br>
+
+                <form method="post">
+
+                  <div class="form-outline mb-4">
+                    <input type="email" id="email" class="form-control"
+                    name="email"
+                      placeholder="Enter Email address">
+                    <label class="form-label" >Email</label>
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <input type="password" id="password" name="password" class="form-control" placeholder="Enter Password">
+                    <label class="form-label">Password</label>
+                  </div>
+
+                  <div class="text-center pt-1 mb-5 pb-1">
+                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="login" name="login" id="login" value="login">Login</button>
+                  </div>
+                </form>
+
+              </div>
+            </div>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</body>
+<script>
+<!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.2.0/mdb.min.js"
+></script>
+</script>
+</html>
