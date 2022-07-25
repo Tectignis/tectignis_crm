550c4ba4e5a28b30f3ec7762b41f6b2eff700484
Madhuri Dere <madhuridere2018@gmail.com>
2022-07-25 16:13:47 +0530

    changes

--- admin/action.php
+++ admin/action.php
@@ -13,16 +13,7 @@ if(isset($_GET['delidd'])){
     }
 ?>
 
-<?php
-//lead delete
-if(isset($_GET['delid'])){
-    $id=mysqli_real_escape_string($conn,$_GET['delid']);
-    $sql=mysqli_query($conn,"delete from lead where id='$id'");
-    if($sql=1){
-        header("location:lead.php");
-    }
-    }
-?>
+
 
 <?php
 //client status
