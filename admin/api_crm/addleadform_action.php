a32af3ce6f748a0bfbd85938586d2c2b2db20ee9
Madhuri Dere <madhuridere2018@gmail.com>
2022-07-25 15:38:49 +0530

    c

--- admin/api_crm/addleadform_action.php
+++ admin/api_crm/addleadform_action.php
@@ -1,6 +1,18 @@
 <?php
 include("./config.php");
 ?>
+<?php
+//lead delete
+if(isset($_GET['delid'])){
+    $id=mysqli_real_escape_string($conn,$_GET['delid']);
+    $sql=mysqli_query($conn,"delete from lead where id='$id'");
+    if($sql=1){
+        header("location:lead.php");
+    }
+    }
+?>
+
+
 
 <?php
 //leads post
