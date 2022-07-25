3b38e1b5d2546f6ac83256b5970bb652e161b2e7
Madhuri Dere <madhuridere2018@gmail.com>
2022-07-25 16:32:38 +0530

    s

--- admin/action.php
+++ admin/action.php
@@ -2,16 +2,6 @@
 
 
 
-<?php
-//client delete
-if(isset($_GET['delidd'])){
-    $id=mysqli_real_escape_string($conn,$_GET['delidd']);
-    $sql=mysqli_query($conn,"delete from client where Client_code='$id'");
-    if($sql=1){
-        header("location:client.php");
-    }
-    }
-?>
 
 
 
