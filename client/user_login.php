dd654438e4b2d6662a4ce102216f57fc4c1dccfe
kajal281199 <97589538+kajal281199@users.noreply.github.com>
2022-07-28 15:23:21 +0530

    gg

--- client/user_login.php
+++ client/user_login.php
@@ -31,7 +31,7 @@ else{
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Admin CRM | Login</title>
-        <!-- Font Awesome -->
+
 <link
   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
   rel="stylesheet"
