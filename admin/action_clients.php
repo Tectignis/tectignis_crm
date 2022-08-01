81e4655974a6c78c63d4acb9b4ff190ee708fc44
keshridivya <dkeshari094@gmail.com>
2022-08-01 12:05:34 +0530

    c

--- admin/action_clients.php
+++ admin/action_clients.php
@@ -2,33 +2,6 @@
 session_start();
 include("config.php");
 ?>
-<?php
-// if(isset($_POST['submit'])){
-//     $name=$_POST['name'];
-//     $email=$_POST['email'];
-//     $number=$_POST['number'];
-//     $category=$_POST['category'];
-//     $status=$_POST['Activited'];
-//     $image=$_FILES['image']['name'];
-//     $image_tmp=$_FILES['image']['tmp_name'];
-//     $loc="dist/img/".$image;
-//     move_uploaded_file($image_tmp,$loc);
-
-    
-   
-
-//     $sql=mysqli_query($conn,"INSERT INTO `client`(`Authorized_Name`, `Email`, `Mobile_Number`, `Category`,`image`) 
-//     VALUES ('$name','$email','$number','$category','$image')");
-    
-
-//     if($sql==1){
-//         echo "Saved!", "data successfully submitted", "success";
-//         header("location:clients.php");
-//     }else {
-//         echo '<script>alert("oops...somthing went wrong");</script>';
-//     }
-// }
-?>
 
 <?php
 //client delete
@@ -215,7 +188,7 @@ if( mail($sendTo,$subject,$emailText, "From:" .$from)){
 
     if($sql==1){
         echo '<script>alert("data successfully submitted");</script>';
-        header("location:../client.php");
+        header("location:clients.php");
     }else {
         echo '<script>alert("oops...somthing went wrong");</script>';
     }
