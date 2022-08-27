7d4a9f6e99076eed217af9fe531152f8871d970c
keshridivya <dkeshari094@gmail.com>
2022-08-27 10:21:27 +0530

    c

--- pdf.php
+++ pdf.php
@@ -14,21 +14,21 @@ function fetch_customer_data($conn)
   <table class="table table-striped table-bordered">
    <tr>
     <th>Name</th>
-    <th>Address</th>
-    <th>City</th>
-    <th>Postal Code</th>
-    <th>Country</th>
+    <th>Firm_Name</th>
+    <th>Mobile_Number</th>
+    <th>Requirement</th>
+    <th>package</th>
    </tr>
  ';
  foreach($result as $row)
  {
   $output .= '
    <tr>
-    <td>'.$row["CustomerName"].'</td>
-    <td>'.$row["Address"].'</td>
-    <td>'.$row["City"].'</td>
-    <td>'.$row["PostalCode"].'</td>
-    <td>'.$row["Country"].'</td>
+    <td>'.$row["Client_Name"].'</td>
+    <td>'.$row["Firm_Name"].'</td>
+    <td>'.$row["Mobile_Number"].'</td>
+    <td>'.$row["Requirement"].'</td>
+    <td>'.$row["package"].'</td>
    </tr>
   ';
  }
@@ -52,6 +52,30 @@ if(isset($_POST["action"]))
  <head>
   <title>Create Dynamic PDF Send As Attachment with Email in PHP</title>
   <script src="jquery.min.js"></script>
+  <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+    <!-- summernote -->
+    <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+
+    <link rel="stylesheet" href="plugins/select2/css/select2.min.css">
+    <link rel="stylesheet" href="plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+
   <link rel="stylesheet" href="bootstrap.min.css" />
   <script src="bootstrap.min.js"></script>
  </head>
