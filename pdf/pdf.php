4d282659a64667d0cf11928dc7bcf5c292b9d79d
keshridivya <dkeshari094@gmail.com>
2022-08-27 11:34:32 +0530

    c

--- pdf/pdf.php
+++ pdf/pdf.php
@@ -31,7 +31,7 @@ function fetch_customer_data($conn)
     <td>'.$row["package"].'</td>
    </tr>
   ';
-  
+
  }
  $output .= '
   </table>
@@ -44,7 +44,36 @@ if(isset($_POST["action"]))
 {
  include('pdf.php');
  $file_name = md5(rand()) . '.pdf';
- header('location:http://wa.me/send?phone=9199&attachment=c://users/downloads/'.$file_name);
+ $html_code = '<link rel="stylesheet" href="bootstrap.min.css">';
+ $html_code .= fetch_customer_data($connect);
+ $pdf = new Pdf();
+ $pdf->load_html($html_code);
+ $pdf->render();
+ $file = $pdf->output();
+ file_put_contents($file_name, $file);
+ 
+ require 'class/class.phpmailer.php';
+ $mail = new PHPMailer;
+ $mail->IsSMTP(); 
+ $mail->Host = 'smtmjk.hostinger.com';  //Sets the SMTP hosts of your Email hosting, this for Godaddy
+ $mail->Port = '465';        //Sets the default SMTP server port
+ $mail->SMTPAuth = true;       //Sets SMTP authentication. Utilizes the Username and Password variables
+ $mail->Username = 'admin';     //Sets SMTP username
+ $mail->Password = 'Admin#07';     //Sets SMTP password
+ $mail->SMTPSecure = '';       //Sets connection prefix. Options are "", "ssl" or "tls"
+ $mail->From = 'divyani.keshari@tectignis.in';   //Sets the From email address for the message
+ $mail->FromName = 'Webslesson.info';   //Sets the From name of the message
+ $mail->AddAddress('snehal7039@gmail.com', 'Name');  //Adds a "To" address
+ $mail->WordWrap = 50;       //Sets word wrapping on the body of the message to a given number of characters
+ $mail->IsHTML(true);       //Sets message type to HTML    
+ $mail->AddAttachment($file_name);         //Adds an attachment from a path on the filesystem
+ $mail->Subject = 'Customer Details';   //Sets the Subject of the message
+ $mail->Body = 'Please Find Customer details in attach PDF File.';    //An HTML or plain text message body
+ if($mail->Send())        //Send an Email. Return true on success or false on error
+ {
+  $message = '<label class="text-success">Customer Details has been send successfully...</label>';
+ }
+ unlink($file_name);
 }
 
 ?>
