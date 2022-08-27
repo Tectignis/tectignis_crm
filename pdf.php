3e61b0e29dcaef53dffc0b33bc78e1686745e81c
keshridivya <dkeshari094@gmail.com>
2022-08-27 10:16:53 +0530

    c

--- pdf.php
+++ pdf.php
@@ -1,12 +1,13 @@
 <?php
-include("admin/config.php");
 //index.php
 
 $message = '';
+$connect = new PDO("mysql:host=151.106.124.51;dbname=u188140722_crm1", "u188140722_crm1", "Admin@123");
 function fetch_customer_data($conn)
 {
  $query = "SELECT * FROM LEAD";
- $statement = mysqli_query($conn,$query);
+ $statement = $connect->prepare($query);
+ $statement->execute();
  $result = $statement->fetchAll();
  $output = '
  <div class="table-responsive">
@@ -42,36 +43,7 @@ if(isset($_POST["action"]))
 {
  include('pdf.php');
  $file_name = md5(rand()) . '.pdf';
- $html_code = '<link rel="stylesheet" href="bootstrap.min.css">';
- $html_code .= fetch_customer_data($connect);
- $pdf = new Pdf();
- $pdf->load_html($html_code);
- $pdf->render();
- $file = $pdf->output();
- file_put_contents($file_name, $file);
  
- require 'class/class.phpmailer.php';
- $mail = new PHPMailer;
- $mail->IsSMTP();        //Sets Mailer to send message using SMTP
- $mail->Host = 'smtpout.secureserver.net';  //Sets the SMTP hosts of your Email hosting, this for Godaddy
- $mail->Port = '80';        //Sets the default SMTP server port
- $mail->SMTPAuth = true;       //Sets SMTP authentication. Utilizes the Username and Password variables
- $mail->Username = 'xxxxxxxxxx';     //Sets SMTP username
- $mail->Password = 'xxxxxxxxxx';     //Sets SMTP password
- $mail->SMTPSecure = '';       //Sets connection prefix. Options are "", "ssl" or "tls"
- $mail->From = 'info@webslesson.info';   //Sets the From email address for the message
- $mail->FromName = 'Webslesson.info';   //Sets the From name of the message
- $mail->AddAddress('web-tutorial@programmer.net', 'Name');  //Adds a "To" address
- $mail->WordWrap = 50;       //Sets word wrapping on the body of the message to a given number of characters
- $mail->IsHTML(true);       //Sets message type to HTML    
- $mail->AddAttachment($file_name);         //Adds an attachment from a path on the filesystem
- $mail->Subject = 'Customer Details';   //Sets the Subject of the message
- $mail->Body = 'Please Find Customer details in attach PDF File.';    //An HTML or plain text message body
- if($mail->Send())        //Send an Email. Return true on success or false on error
- {
-  $message = '<label class="text-success">Customer Details has been send successfully...</label>';
- }
- unlink($file_name);
 }
 
 ?>
