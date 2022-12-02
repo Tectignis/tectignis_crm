d74c1f94b92e04fa7596ee974d61acd2be0890db
keshridivya <dkeshari094@gmail.com>
2022-12-02 13:24:13 +0530

    c

--- client/app/client/auth/dashboard/package.php
+++ client/app/client/auth/dashboard/package.php
@@ -41,8 +41,10 @@ $packageId=$_GET['packageId'];
     //   }else{
     //    $sql1=mysqli_query($conn,"INSERT INTO `remarks`(`remark`,`lead_id`,`date_time`) VALUES ('$remark','$id','$date')");
     //   }
+    // $baseurl= "https://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
       if($sql==1){
           echo "Saved!", "data successfully submitted", "success";
+          header('location:'.$packageId);
       }else {
           echo '<script>alert("oops...somthing went wrong");</script>';
       }
@@ -59,7 +61,7 @@ $packageId=$_GET['packageId'];
 <!-- BEGIN: Head-->
 
 <head>
-    <base href="http://localhost/tectignis_crm/client/app/client/auth/dashboard/">
+    <base href="http://localhost:8000/tectignis_crm/client/app/client/auth/dashboard/">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
