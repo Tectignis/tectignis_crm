ee8054e4dd1a1a98a64751fe801f4b1c5de0c4b0
monikagore <mobnikgore5=6@m=gmail.com>
2022-12-02 10:27:50 +0530

    ch

--- client/app/client/auth/dashboard/package.php
+++ client/app/client/auth/dashboard/package.php
@@ -138,7 +138,7 @@ $packageId=$_GET['packageId'];
                             <h2 class="content-header-title float-start mb-0">Leads</h2>
                             <div class="breadcrumb-wrapper">
                                 <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="index.html">Clients</a>
+                                    <li class="breadcrumb-item"><a href="index.html">Packages</a>
                                     </li>
                                     <li class="breadcrumb-item"><a href="#">Leads</a>
                                     </li>
