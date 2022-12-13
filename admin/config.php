07c0e89ad52ae441fc1d1c983a13de79715f1474
keshridivya <dkeshari094@gmail.com>
2022-12-13 11:25:00 +0530

    c

--- admin/config.php
+++ admin/config.php
@@ -3,4 +3,5 @@ $conn=mysqli_connect("151.106.124.51","u188140722_crm1","Admin@123","u188140722_
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+
 ?>
