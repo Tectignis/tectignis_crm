84608c7dcc39ce75f7fbdf608a4237dcb9214609
keshridivya <dkeshari094@gmail.com>
2022-12-13 10:52:26 +0530

    c

--- admin/clients.php
+++ admin/clients.php
@@ -56,7 +56,7 @@ if(isset($_POST["submi"])){
     <!-- END: Main Menu-->
 
     <!-- BEGIN: Content-->
-    <div class="app-content content ">
+    <div class="app-content content">
         <div class="content-overlay"></div>
         <div class="header-navbar-shadow"></div>
         <div class="content-wrapper container-xxl p-0">
